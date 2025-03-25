a71f6cac41fa7267315207d22ff38d37a5d82aab
Ngọc Hùng <huynhngochung12092001@gmail.com>
2025-03-25 22:34:08 +0700

    feat: add `AlbumSetting` constant

---
+++ app/Constants/AlbumSetting.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\App\Constants;
+
+class AlbumSetting
+{
+    /** @var int The maximum number of highlighted albums. */
+    public const MAX_HIGHLIGHT_ALBUMS = 9;
+}
